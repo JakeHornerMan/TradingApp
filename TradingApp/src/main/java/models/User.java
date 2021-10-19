4a99b9322077bcba30e9c2153366796ad18f0227
Amy <74540344+13-8billion@users.noreply.github.com>
2021-10-19 14:49:26 +0100

    user model

---
+++ TradingApp/src/main/java/models/User.java
@@ -0,0 +1,58 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int user_id;
+    private String user_email;
+    private String user_password;
+    private int wallet_id;
+
+    public User(int user_id, String user_email, String user_password, int wallet_id) {
+        this.user_id = user_id;
+        this.user_email = user_email;
+        this.user_password = user_password;
+        this.wallet_id = wallet_id;
+    }
+
+    public User() {
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    public int getWallet_id() {
+        return wallet_id;
+    }
+
+    public void setWallet_id(int wallet_id) {
+        this.wallet_id = wallet_id;
+    }
+}
