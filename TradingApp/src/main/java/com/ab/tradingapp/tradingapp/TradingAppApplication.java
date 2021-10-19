3f400c9829c6778fea0e4d64e2979679b5ca4649
Amy <74540344+13-8billion@users.noreply.github.com>
2021-10-19 10:38:30 +0100

    initial commit

---
+++ TradingApp/src/main/java/com/ab/tradingapp/tradingapp/TradingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ab.tradingapp.tradingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAppApplication.class, args);
+	}
+
+}
---
+++ TradingApp/src/test/java/com/ab/tradingapp/tradingapp/TradingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.tradingapp.tradingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
