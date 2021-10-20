4a7162a5e3757befa837979c1a96f3fe76c1549a
Yakub54 <90615272+Yakub54@users.noreply.github.com>
2021-10-20 11:12:36 +0100

    StocksEntity

--- TradingApp/src/main/java/com/ab/tradingapp/models/Stocks.java
+++ TradingApp/src/main/java/com/ab/tradingapp/models/Stocks.java
@@ -5,73 +5,83 @@ import javax.persistence.Id;
 
 @Entity
 public class Stocks {
-
+	
 	@Id
 	private int stock_id;
-	private String stock_name;
-	private double stock_value;
-	private double stock_fee;
+	private String stcok_name; 
+	private boolean stock_bse;
+	private boolean stock_nse;
+	private boolean stock_tyo;
 	
-	
-	public Stocks () {}
+	public Stocks() {}
 
-
-	public Stocks(int stock_id, String stock_name, double stock_value, double stock_fee) {
+	public Stocks(int stock_id, String stcok_name, boolean stock_bse, boolean stock_nse, boolean stock_tyo) {
 		super();
 		this.stock_id = stock_id;
-		this.stock_name = stock_name;
-		this.stock_value = stock_value;
-		this.stock_fee = stock_fee;
+		this.stcok_name = stcok_name;
+		this.stock_bse = stock_bse;
+		this.stock_nse = stock_nse;
+		this.stock_tyo = stock_tyo;
 	}
-
 
 	public int getStock_id() {
 		return stock_id;
 	}
 
-
 	public void setStock_id(int stock_id) {
 		this.stock_id = stock_id;
 	}
 
-
-	public String getStock_name() {
-		return stock_name;
+	public String getStcok_name() {
+		return stcok_name;
 	}
 
-
-	public void setStock_name(String stock_name) {
-		this.stock_name = stock_name;
+	public void setStcok_name(String stcok_name) {
+		this.stcok_name = stcok_name;
 	}
 
-
-	public double getStock_value() {
-		return stock_value;
+	public boolean isStock_bse() {
+		return stock_bse;
 	}
 
-
-	public void setStock_value(double stock_value) {
-		this.stock_value = stock_value;
+	public void setStock_bse(boolean stock_bse) {
+		this.stock_bse = stock_bse;
 	}
 
-
-	public double getStock_fee() {
-		return stock_fee;
+	public boolean isStock_nse() {
+		return stock_nse;
 	}
 
-
-	public void setStock_fee(double stock_fee) {
-		this.stock_fee = stock_fee;
+	public void setStock_nse(boolean stock_nse) {
+		this.stock_nse = stock_nse;
 	}
 
+	public boolean isStock_tyo() {
+		return stock_tyo;
+	}
+
+	public void setStock_tyo(boolean stock_tyo) {
+		this.stock_tyo = stock_tyo;
+	}
 
 	@Override
 	public String toString() {
-		return "Stocks [stock_id=" + stock_id + ", stock_name=" + stock_name + ", stock_value=" + stock_value
-				+ ", stock_fee=" + stock_fee + "]";
+		return "Stocks [stock_id=" + stock_id + ", stcok_name=" + stcok_name + ", stock_bse=" + stock_bse
+				+ ", stock_nse=" + stock_nse + ", stock_tyo=" + stock_tyo + "]";
 	}
 	
 	
+	
+	
+
+//	CREATE TABLE `Stocks` (
+//			`stock_id` INT NOT NULL AUTO_INCREMENT,
+//			`stock_name` varchar(255) NOT NULL,
+//			`stock_bse` BOOLEAN NOT NULL DEFAULT false,
+//			`stock_nse` BOOLEAN NOT NULL DEFAULT false,
+//			`stock_tyo` BOOLEAN NOT NULL DEFAULT false,
+//			PRIMARY KEY (`stock_id`)
+//		);
 	
 
 }
